309c48169fd17045ba713c094ffc0d932ee4618a
brunafernandes1959 <fernandesbruna1959@hotmail.com>
2021-07-06 19:47:30 -0300

    button edit and delete

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { MdDelete, MdModeEdit } from 'react-icons/md';
+import { ToastContainer, toast } from 'react-toastify';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 import useForm from '../../../hooks/useForm';
+import config from '../../../config';
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -11,6 +15,50 @@ function CadastroCategoria() {
     descricao: '',
     cor: '',
   };
+
+  const Th = styled.th`
+  text-align: center !important;
+`;
+
+  const Td = styled.td`
+  text-align: center !important;
+
+  .btn-delete {
+    background: var(--danger);
+    width: 80px;
+    height: 54px;
+    border: none;
+    margin: 5px 15px 20px 0;
+  }
+
+  .btn-edit {
+    background: var(--backEnd);
+    width: 80px;
+    height: 54px;
+    border: none;
+    margin: 5px 15px 20px 0;
+  }
+`;
+
+  const Table = styled.table`
+  border: 1px solid #2a7ae4;
+  border-collapse: collapse;
+  width: 100%;
+  margin: 20px 0;
+
+  thead tr th {
+    padding: 15px;
+    border: 1px solid #2a7ae4;
+  }
+  thead {
+    border-bottom: 1px solid #2a7ae4;
+  }
+  td,
+  th {
+    text-align: left;
+    padding: 10px;
+  }
+`;
 
   const { handleChange, values, clearForm } = useForm(valoresIniciais);
 
@@ -28,6 +76,23 @@ function CadastroCategoria() {
         ]);
       });
   }, []);
+
+  async function handleDelete(id) {
+    try {
+      await fetch(`${config.URL_APP}/categorias/${id}`, {
+        method: 'DELETE',
+      });
+
+      const updatedList = categorias.filter((item) => item.id !== id);
+      setCategorias(updatedList);
+
+      toast.error('Categoria apagada com sucesso!');
+    } catch (error) {
+      toast.error(
+        'Não foi possivél apagar. Entre em contato com o administrador.',
+      );
+    }
+  }
 
   return (
     <PageDefault>
@@ -73,9 +138,8 @@ function CadastroCategoria() {
         <Button type="submit">
           Cadastrar
         </Button>
-      </form>
 
-      
+      </form>
 
       <Table>
         <thead>
@@ -87,27 +151,26 @@ function CadastroCategoria() {
           </tr>
         </thead>
         <tbody>
-          {categories.map((category, index) => {
-            return (
-              <tr key={index}>
-                <td>{category.titulo}</td>
-                <td>{category.descricao}</td>
-                <Td>
-                  <Button className="btn-edit">
-                    <MdModeEdit size={25} />
-                  </Button>
-                </Td>
-                <Td>
-                  <Button className="btn-delete">
-                    <MdDelete
-                      size={25}
-                      onClick={() => handleDelete(category.id)}
-                    />
-                  </Button>
-                </Td>
-              </tr>
-            )
-          })}
+          {categorias.map((category, index) => (
+            <tr key={index}>
+              <td>{category.titulo}</td>
+              <td>{category.descricao}</td>
+              <Td>
+                <Button className="btn-edit">
+                  <MdModeEdit size={25} />
+                </Button>
+              </Td>
+              <Td>
+                <Button className="btn-delete">
+                  <MdDelete
+                    size={25}
+                    onClick={() => handleDelete(category.id)}
+                  />
+                </Button>
+              </Td>
+              <ToastContainer position="top-right" autoClose={3000} />
+            </tr>
+          ))}
         </tbody>
       </Table>
       <Link to="/">
